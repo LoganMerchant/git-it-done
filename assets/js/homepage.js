1c7289e8e1666040bcedb4894d56eda09e62bf40
Logan Merchant <merchantclogan@gmail.com>
2020-08-31 22:59:26 -0600

    update function for fetching user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,13 @@
-var getUserRepos = function() {
-    // fetch the repos under the octocat user...then pass response through a function
-    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-        // the fetched response should be formatted as JSON, then passed data as a callback. 
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
     });
 };
 
-getUserRepos();
+getUserRepos("amazon");
