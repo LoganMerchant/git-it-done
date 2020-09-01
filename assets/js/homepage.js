828498fad50cb3b5c525cc483116530073614b1b
Logan Merchant <merchantclogan@gmail.com>
2020-08-31 23:44:03 -0600

    add form submission handler

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,8 @@
+// select any element with an id of `user-form`
+var userFormEl = document.querySelector("#user-form");
+// select any element with an id of `username`
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +15,24 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos("amazon");
+var formSubmitHandler = function(event) {
+    // prevent the page from browser from refreshing/submitting the form
+    event.preventDefault();
+
+    // get and trim the value of whatever the user input in the form
+    var username = nameInputEl.value.trim();
+
+    // if username returns true, i.e. there is a name there...
+    if (username) {
+        // perform the `getUserRepos` function with `username` as the parameter
+        getUserRepos(username);
+        // empty the input field in the form
+        nameInputEl.value = "";
+    // if there is no username when the form is submitted, alert the user
+    } else {
+        alert("Please enter a GitHub username.");
+    };
+};
+
+// when the userFormEl is submitted, perform the `formSubmitHandler` function.
+userFormEl.addEventListener("submit", formSubmitHandler);
