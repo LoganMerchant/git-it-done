8e60918972e5f274201c3c6a9a22b154b0e08d05
Logan Merchant <merchantclogan@gmail.com>
2020-08-31 22:51:41 -0600

    hardcode example of fetch api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    // fetch the repos under the octocat user...then pass response through a function
+    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+        // the fetched response should be formatted as JSON, then passed data as a callback. 
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
