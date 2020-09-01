13df2d32d24659d4420c316aa957c1f7502dc872
Logan Merchant <merchantclogan@gmail.com>
2020-08-31 23:28:05 -0600

    add search form HTML

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-    // make a request to the url
+    // make a request to the url (then is asynchronous)
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
